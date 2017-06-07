8a9b9c59337c8c653364d69c08bc8de599ec2b17
Madsost <m@madsostergaard.dk>
2017-06-07 12:23:56 +0200

    GUI v1

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,12 +1,41 @@
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
 
+	private Stage primaryStage;
+	private BorderPane rootLayout;
+
 	@Override
 	public void start(Stage primaryStage) {
-		
+		try {
+			this.primaryStage = primaryStage;
+
+			this.primaryStage.setTitle("GUI-Mockup");
+
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("/RootLayout.FXML"));
+
+			rootLayout = (BorderPane) loader.load();
+			Scene scene = new Scene(rootLayout);
+			primaryStage.setScene(scene);
+			
+			primaryStage.show();
+
+			loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("/EKGView.FXML"));
+			AnchorPane ekgOverview = (AnchorPane) loader.load();
+			rootLayout.setCenter(ekgOverview);
+			
+
+		} catch (Exception e) {
+		}
+
 	}
 
 	public static void main(String[] args) {
