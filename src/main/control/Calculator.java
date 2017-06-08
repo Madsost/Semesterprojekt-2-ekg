00618647af1f8fd6db7a933859087ce8b437c0ef
Madsost <m@madsostergaard.dk>
2017-06-08 09:05:25 +0200

    Med henvisning til Li tan

--- src/main/control/Calculator.java
+++ src/main/control/Calculator.java
@@ -24,7 +24,21 @@ public class Calculator {
 			calcDataset.add(Filter.doFilter(data));
 		}
 
-		// z-cross algoritme (Li tan side ??)
+		// z-cross algoritme (Li tan side 369)
+		// MATLAB kode:
+		/*
+		 * zcross = 0.0; threshold=0.8; for n= 2:length(y2) pre_sign = -1;
+		 * cur_sign = -1; if 
+		 * y2(n-1)>threshold pre_sign = 1; 
+		 * end 
+		 * if
+		 * y2(n)>threshold cur_sign = 1; 
+		 * end 
+		 * zcross = zcross + abs(cur_sign-pre_sign)/2; 
+		 * end 
+		 * 
+		 * rate = 60/(length(y2)/300)*(zcross/2)
+		 */
 
 		return result;
 	}
