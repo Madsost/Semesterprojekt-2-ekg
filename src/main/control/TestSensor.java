82252c9f56b03cf6d1a16e2ff31d298618bdfdba
Madsost <m@madsostergaard.dk>
2017-06-13 12:49:19 +0200

    Nyt dataset med samplerate 250 Hz (4 ms mellem målinger)

--- src/main/control/TestSensor.java
+++ src/main/control/TestSensor.java
@@ -3,38 +3,18 @@ package main.control;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
-public class TestSensor implements Sensor, Observed {
+public class TestSensor extends Thread implements Sensor {
+	// private ArrayList<Double>
 
-	// sætter instansen op af Queue så det er den samme som databaseConn tilgår
+	// sï¿½tter instansen op af Queue sï¿½ det er den samme som databaseConn tilgï¿½r
 	Queue q = Queue.getInstance();
-	
+
 	@Override
 	public void init() {
 		// TODO Auto-generated method stub
 	}
 
-	@Override
-	public ArrayList<Double> getValues() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void attachListener(ActionListener l) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void detachListener(ActionListener l) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void notification(String string) {
-		// TODO Auto-generated method stub
+	public void run(){
 		
 	}
-
 }
