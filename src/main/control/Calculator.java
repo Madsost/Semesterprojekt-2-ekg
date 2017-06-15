d3ad6758bd974106bcaed15a9ac195de4e3e1c08
Madsost <m@madsostergaard.dk>
2017-06-15 09:28:41 +0200

    Blah

--- src/main/control/Calculator.java
+++ src/main/control/Calculator.java
@@ -20,14 +20,19 @@ public class Calculator {
 	}
 
 	public int calculatePulse() {
-		ArrayList<Double> inputDataset = null; // DatabaseConn.getData(1000);
+		ArrayList<Integer> inputDataset = null; // DatabaseConn.getData(1000);
 		int result = 0;
-		for (double data : inputDataset) {
+		for (int data : inputDataset) {
 			calcDataset.add(Filter.doFilter(data));
 		}
 
 		// z-cross algoritme (Li tan side 369)
 		// MATLAB kode:
+		
+		double zcross = 0.0;
+		double threshold = 800;
+		//for(int n = 2; n<)
+		
 		/*
 		 * zcross = 0.0; threshold=0.8; for n= 2:length(y2) pre_sign = -1;
 		 * cur_sign = -1; if y2(n-1)>threshold pre_sign = 1; end if
